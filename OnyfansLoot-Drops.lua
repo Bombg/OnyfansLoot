0990b9b3eeaaab2d000e29f0e4ba351651bb20af
Joshua Gottlieb <Bombgh@gmail.com>
2024-08-20 04:45:34 -0500

    remove todo

--- OnyfansLoot-Drops.lua
+++ OnyfansLoot-Drops.lua
@@ -5,7 +5,7 @@ OfLootDrops:RegisterEvent("CHAT_MSG_SYSTEM")
 OfLootDrops:SetScript("OnEvent", function ()
     local chatMsg = arg1
     local lineId = arg2 -- If this is unique across clients I can probably use this to my advantage
-    if IsInRaid() and event == "CHAT_MSG_LOOT" then -- TODO: Add back raid check
+    if IsInRaid() and event == "CHAT_MSG_LOOT" then
         local regex = "(%a-) receive"
         if string.find(chatMsg, regex) then
             local playerName, itemLink = GetNameItemLinkFromLootMsg(chatMsg)
