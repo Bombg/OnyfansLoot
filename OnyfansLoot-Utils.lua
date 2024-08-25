ddf14a9bf6c8e1ba543f1303a020e69857a4c2d3
Joshua Gottlieb <Bombgh@gmail.com>
2024-08-25 02:56:24 -0500

    updating blacklist

--- OnyfansLoot-Utils.lua
+++ OnyfansLoot-Utils.lua
@@ -236,7 +236,8 @@ end
 function util:IsItemBlackListed(itemName)
     local itemBlackList = {
         "idol of the sun","idol of war","blue qiraji resonating crystal","idol of life","idol of death","idol of rebirth",
-        "idol of strife","green qiraji resonating crystal","idol of night","large brilliant shard","idol of the sage", "yellow qiraji resonating crystal"
+        "idol of strife","green qiraji resonating crystal","idol of night","large brilliant shard","idol of the sage", "yellow qiraji resonating crystal",
+        "fiery core", "lava core", "book: gift of the wild"
     }
     local isBlacklisted = false
     for i, v in ipairs(itemBlackList) do
