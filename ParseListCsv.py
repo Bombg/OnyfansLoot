afee4871949defe7ab6a0eaf3f65495ec321ea97
Joshua Gottlieb <Bombgh@gmail.com>
2024-08-26 17:54:23 -0500

    adding changes before removal, in case I want to retrieve in the future

--- ParseListCsv.py
+++ ParseListCsv.py
@@ -1,5 +1,6 @@
 from slpp import slpp as lua
 import csv
+import re
 
 namePosition = 0
 lootPosition = 6
@@ -8,30 +9,50 @@ csvArray = []
 versionArr = []
 
 
-listVersion = 2
-listDateTime = "8-15-24:10:34"
+listVersion = 5
+listDateTime = "8-18-24:20:05"
+
+def cleanString(string: str):
+    newString = string.strip()
+    newString = newString.strip("[")
+    newString = newString.strip("]")
+    newString = newString.rstrip(",")
+    newString = newString.lower()
+    return newString
 
 
-with open("OFLootList-8-15-24.csv", mode = 'r') as file:
+with open("OFLootList-8-18-24.csv", mode = 'r') as file:
     csvFile = csv.reader(file)
     headers = next(csvFile)
     for line in csvFile:
         csvArray.append(line)
         for i in range(lootPosition,len(line)):
-            if ''.join(line[i]).strip():
-                lootToName[line[i].strip().strip("[").strip("]").lower()] = []
-                print(line[i].strip().strip("[").strip("]").lower())
+            if ''.join(line[i]).strip(): ## Checking for empty cell
+                if "?" in line[i]:
+                    loots = line[i].split("?")
+                    for string in loots:
+                        lootToName[cleanString(string)] = []
+                else:
+                    lootToName[cleanString(line[i])] = []
+                    print(cleanString(line[i]))
 
-for i in range(lootPosition,25):
+for i in range(lootPosition,24):
     rollString = {}
     for line in csvArray:
         if ''.join(line[i]).strip():
-            if line[i].strip().lower() not in rollString:
-                rollString[line[i].strip().lower()] =  line[0] + ", "
+            if "?" in line[i]:
+                loots = line[i].split("?")
+                for loot in loots:
+                    if cleanString(line[i]) not in rollString:
+                        rollString[cleanString(loot)] =  line[0] + ", "
+                    else:
+                        rollString[cleanString(loot)] = rollString[cleanString(loot)] + line[0] + ", "
+            elif cleanString(line[i]) not in rollString:
+                rollString[cleanString(line[i])] =  line[0] + ", "
             else:
-                rollString[line[i].strip().lower()] = rollString[line[i].strip().lower()] + line[0] + ", "
+                rollString[cleanString(line[i])] = rollString[cleanString(line[i])] + line[0] + ", "
     for k, v in rollString.items():
-        lootToName[k.strip("[").strip("]")].append(v.strip().rstrip(','))
+        lootToName[cleanString(k)].append(cleanString(v))
 
 versionArr.append(listVersion)
 versionArr.append(listDateTime)
